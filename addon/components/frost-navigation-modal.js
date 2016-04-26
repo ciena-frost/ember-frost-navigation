72d91666e704323ff883fcb0b8c047e1bf42a704
mhyndman <mhyndman@ciena.com>
2016-04-26 09:52:21 -0400

    fixes for app bar navigation

--- addon/components/frost-navigation-modal.js
+++ addon/components/frost-navigation-modal.js
@@ -1,6 +1,6 @@
 import Ember from 'ember'
 
-const {Component, computed, isEmpty, on} = Ember
+const {Component, computed, isEmpty} = Ember
 
 export default Component.extend({
   layoutName: computed('navigationTemplate', function () {
@@ -9,22 +9,6 @@ export default Component.extend({
       return 'navigation'
     }
     return navigationTemplate
-  }),
-
-  onInsert: on('didInsertElement', function () {
-    Ember.$('.frost-application-bar .frost-button.navigation')
-    .prop('disabled', true)
-    Ember.$('.frost-application-bar')
-    .on('click.frost-navigation', () => {
-      this.sendAction('dismiss')
-    })
-  }),
-
-  onDestroy: on('willDestroyElement', function () {
-    Ember.$('.frost-application-bar .frost-button.navigation')
-    .prop('disabled', false)
-    Ember.$('.frost-application-bar')
-    .off('click.frost-navigation')
   }),
 
   actions: {
