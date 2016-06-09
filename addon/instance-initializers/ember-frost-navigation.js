4f143ea53137cb06fbac6c167bc6903a4643e477
Seena <seenarowhani95@gmail.com>
2016-06-08 22:51:25 -0400

    code refactor

--- addon/instance-initializers/ember-frost-navigation.js
+++ addon/instance-initializers/ember-frost-navigation.js
@@ -5,33 +5,25 @@ export default {
   name: 'ember-frost-navigation',
   initialize (instance) {
     let navigation = instance.lookup('service:frost-navigation')
-    let transitionService = appInstance.lookup('service:liquid-fire-transitions')
+    let transitionService = instance.lookup('service:liquid-fire-transitions')
 
     transitionService.map(transitions)
     Ember.RouterDSL.prototype.nav = function (componentName, opts = {}) {
       let self = this
       return new Ember.RSVP.Promise(function (resolve, reject) {
-        Ember.assert('opts.navType must be either \'category\' or \'app\'',
-          opts.navType !== 'category' || opts.navType !== 'app')
-        Ember.assert('opts.type must be either \'engine\' or \'route\'',
-          opts.type !== 'engine' || opts.type !== 'route')
+        opts.name = opts.name || componentName
         self[opts.type === 'engine' ? 'mount' : 'route'](componentName, opts)
-
-        let r = {
-          category () {
-            return navigation.registerCategory(opts.name || componentName,
-              opts.columns || [])
-          },
-          app () {
-            return navigation.registerApp(opts.name || componentName,
-              opts.columnTitle,
-              opts.icon,
-              opts.name,
-              opts.description,
-              opts.path)
-          }
-        }[opts.navType]()
-        r ? resolve(r) : reject(r)
+        try {
+          Ember.assert('opts.navType must be either \'category\' or \'app\'',
+            opts.navType === 'category' || opts.navType === 'app')
+          Ember.assert('opts.type must be either \'engine\' or \'route\'',
+            opts.type === 'engine' || opts.type === 'route')
+        } catch (e) {
+          reject(e)
+        }
+        navigation.register(opts)
+          .then(resolve)
+          .catch(reject)
       })
     }
   }
--- addon/services/frost-navigation.js
+++ addon/services/frost-navigation.js
@@ -1,136 +1,141 @@
 import Ember from 'ember'
 
 export default Ember.Service.extend({
-  registerCategory (name, columns) {
-    return !!this.categories.push({
-      name,
-      columns
+  _registerMap: {
+    category (config) {
+      return this._registerCategory(config)
+    },
+    app (config) {
+      return this._registerApp(config)
+    }
+  },
+  register (config) {
+    let self = this
+    Ember.assert('Navigation Type must be specified', config.navType)
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      self._registerMap[config.navType].call(self, config)
+        .then(resolve)
+        .catch(reject)
     })
   },
-  registerApp (categoryName, columnTitle, icon, name, description, path) {
-    let _category = this.categories.find(e => e.name === categoryName)
-    Ember.assert(`Category ${_category} does not exist!`, !_category)
-    let _column = _category.columns.find(e => e.title === columnTitle)
-    Ember.assert(`Column ${_column} does not exist!`, !_category)
-
-    return !!_column.push({
-      icon,
-      name,
-      description,
-      path
+  _registerCategory (config = {}) {
+    let self = this
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      try {
+        Ember.assert('Property \'name\' was not defined.', config.name)
+      } catch (e) {
+        reject(e)
+      }
+      self.categories.push({
+        name: config.name,
+        columns: config.columns || []
+      })
+      resolve(self)
+    })
+  },
+  _registerApp (config = {}) {
+    let self = this
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      try {
+        let _category = self.categories.find(e => e.name === config.categoryName)
+        Ember.assert(`Category ${_category} does not exist!`, _category)
+        let _column = _category.columns.find(e => e.title === config.columnTitle)
+        Ember.assert(`Column ${_column} does not exist!`, _column)
+      } catch (e) {
+        reject(e)
+      }
+      _column.push({
+        icon: config.icon,
+        name: config.name,
+        description: config.description,
+        route: config.route
+      })
+      resolve(self)
     })
   },
   activeCategory: null,
-  categories: [
-    {
-      name: 'Network',
-      columns: [
-        [
-          {
-            title: 'Resources',
-            color: '#009EEF',
-            routes: [
-              {
-                icon: 'sample',
-                name: 'Network elements',
-                description: 'Description',
-                route: 'demo.redesign.nested'
-              },
-              {
-                icon: 'sample',
-                name: 'Equipment',
-                description: 'Description',
-                route: 'demo.redesign'
-              },
-              {
-                icon: 'sample',
-                name: 'Topology map',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Capacity map',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Fiber plant',
-                description: 'Description'
-              }
-            ]
-          }
-        ],
-        [
-          {
-            title: 'Services',
-            color: '#359E35',
-            routes: [
-              {
-                icon: 'sample',
-                name: 'Packet services',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Packet infrastructure',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Photonic/OTN transport',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Links',
-                description: 'Description'
-              }
-            ]
-          }
-        ],
-        [
-          {
-            title: 'Troubleshooting',
-            color: '#009999',
-            routes: [
-              {
-                icon: 'sample',
-                name: 'Alarm browser',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Performance metrics browser',
-                description: 'Description'
-              }
-            ]
-          },
-          {
-            title: 'Glanzer',
-            color: '#009999',
-            routes: [
-              {
-                icon: 'sample',
-                name: 'Alarm browser',
-                description: 'Description'
-              },
-              {
-                icon: 'sample',
-                name: 'Performance metrics browser',
-                description: 'Description'
-              }
-            ]
-          }
-        ]
-      ]
-    },
-    {
-      name: 'System',
-      columns: []
-    },
-    {
-      name: 'Planning',
-      columns: []
-    }
-  ]
+  categories: [{
+    name: 'Network',
+    columns: [
+      [{
+        title: 'Resources',
+        color: '#009EEF',
+        routes: [{
+          icon: 'sample',
+          name: 'Network elements',
+          description: 'Description',
+          route: 'demo.redesign.nested'
+        }, {
+          icon: 'sample',
+          name: 'Equipment',
+          description: 'Description',
+          route: 'demo.redesign'
+        }, {
+          icon: 'sample',
+          name: 'Topology map',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Capacity map',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Fiber plant',
+          description: 'Description'
+        }]
+      }],
+      [{
+        title: 'Services',
+        color: '#359E35',
+        routes: [{
+          icon: 'sample',
+          name: 'Packet services',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Packet infrastructure',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Photonic/OTN transport',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Links',
+          description: 'Description'
+        }]
+      }],
+      [{
+        title: 'Troubleshooting',
+        color: '#009999',
+        routes: [{
+          icon: 'sample',
+          name: 'Alarm browser',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Performance metrics browser',
+          description: 'Description'
+        }]
+      }, {
+        title: 'Glanzer',
+        color: '#009999',
+        routes: [{
+          icon: 'sample',
+          name: 'Alarm browser',
+          description: 'Description'
+        }, {
+          icon: 'sample',
+          name: 'Performance metrics browser',
+          description: 'Description'
+        }]
+      }]
+    ]
+  }, {
+    name: 'System',
+    columns: []
+  }, {
+    name: 'Planning',
+    columns: []
+  }]
 })
