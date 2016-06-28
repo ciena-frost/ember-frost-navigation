d4a7983afd3c60da30cf76a9d7b6d1d92b68a757
Seena Rowhani <seenarowhani95@gmail.com>
2016-06-28 11:18:31 -0400

    Force absolute truth on truthey / falsey assert.

--- addon/services/frost-navigation.js
+++ addon/services/frost-navigation.js
@@ -28,7 +28,7 @@ export default Ember.Service.extend({
     let _actionHandler
     Ember.assert(
       `Action[${item.action}] does not exist on controller: ${controller.toString()}`,
-      _actionHandler = controller.get(item.action)
+      !!(_actionHandler = controller.get(item.action))
     )
     if (item.dismiss) {
       this.set('_activeCategory', null)
