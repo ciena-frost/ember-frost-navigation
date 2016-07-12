12f0448dbde1fb8a0f7cb1c024e24b535e8be324
Seena <seenarowhani95@gmail.com>
2016-07-12 13:02:15 -0400

    max line length - eslint

--- addon/utils/asserts.js
+++ addon/utils/asserts.js
@@ -1,6 +1,9 @@
 let b = '[ember-frost-navigation] - '
 export default {
-  environment: `${b}config/environment must have field frostNavigation.controller defined as the path to your controller (see dummy app for ember-frost-navigation)`,
+  environment: `
+    ${b}config/environment must have field frostNavigation.controller
+    defined as the path to your controller (see dummy app for ember-frost-navigation)
+  `,
   navType: `${b}opts.navType must be either 'category' or 'app'`,
   type: `${b}opts.type must be either 'engine' or 'route'`,
   categoryName: `${b}Property 'name' was not defined.`,
