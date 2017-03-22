a80fcfdd34bfa6a21bee03ddcf2f297635fc0f00
Adam Meadows <adam.meadows@gmail.com>
2017-03-21 20:16:56 -0600

    Fix typo

--- addon/components/nav-category.js
+++ addon/components/nav-category.js
@@ -60,7 +60,7 @@ export default Component.extend({
       frostNavigation.dismiss()
       if (name !== active) { // click on another tab
         run.later(() => {
-          if (this.isDestroying || this.isDestoryed || frostNavigation.isDestroying || frostNavigation.isDestroyed) {
+          if (this.isDestroying || this.isDestroyed || frostNavigation.isDestroying || frostNavigation.isDestroyed) {
             return
           }
 
